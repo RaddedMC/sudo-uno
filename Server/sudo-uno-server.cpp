20a3690c6141c5b3d5b9ebd6d525300d7537944d
RaddedMC <mycooljames@hotmail.com>
2024-04-02 10:01:58 -0400

    feat: start porting mcisaac code

--- Server/sudo-uno-server.cpp
+++ Server/sudo-uno-server.cpp
@@ -1,9 +1,33 @@
 #include "util/util.h"
+#include <thread>
 
 using namespace SudoUno;
 using namespace std;
 
+// As i've done for all the labs, a vector to hold the threads to keep them referenced until shutdown
+vector <thread> threads;
+
 int main() {
     util::log('I', "init sudo-uno-server");
     util::ascii();
+
+    // TODO: Command-line args to set port number and interface bind
+    const string bind_if = "0.0.0.0";
+    const int bind_port = 6969;
+
+    util::log('I', "Using default settings. Will bind to " + bind_if + ":" + to_string(bind_port));
+
+    // Instantiate the socket server
+    // TODO: can we even change the bind if? do we need to?
+    SocketServer serveSock(bind_port);
+    util::log('I', "Socket opened on port " + to_string(bind_port));
+
+    // Loop indefinitely for each request
+    while (true) {
+        util::log('S', "Waiting for a player to connect...");
+        Socket clientSocket(serveSock.Accept());
+        util::log('S', "Connection established! Isolating...");
+
+        // Always wrap connections into their own thread
+    }
 }
--- Server/util/util.h
+++ Server/util/util.h
@@ -1,15 +1,60 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+// TODO: This header file could probably be broken into several smaller ones
 #include <ctime>
 #include <iostream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 namespace SudoUno {
+
     namespace util {
         void ascii();
         void log(char level, const string& msg);
+    }
+
+    namespace network {
+
+        // TODO: needed?
+        class ByteArray {
+            public:
+                vector<char> v;
+
+                // TODO: needed?
+                string ToString(void) const
+                {
+                    string returnValue  ;
+                    for (int i = 0; i < v.size(); i++)
+                        returnValue.push_back(c: v[i]);
+                    return returnValue;
+                }
+
+                // Constructors
+                ByteArray(void){} // Create from nothing
+                ByteArray(string const & input) { // Create from string
+                    for (int i = 0; i < input.size(); i++)
+                        v.push_back(x: input[i]);
+                }
+                ByteArray(void * pointer, int size) { // Create from char*
+                    char * temp = (char*) pointer;
+                    for (int i = 0; i < size; i++)
+                        v.push_back(x: temp[i]);
+                }
+        }
+
+        // TODO: Blockable needs to be included
+        class Socket : public Blockable {
+            private:
+                sockaddr_in socketDescriptor;
+                
+        }
     }
 }
 
