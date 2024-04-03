d51a81d028fe1da893c296fd86fa02a0f13fb04a
RaddedMC <mycooljames@hotmail.com>
2024-04-03 19:36:42 -0400

    feat: Started a few game objects

---
+++ Server/game/game.cpp
@@ -0,0 +1,26 @@
+#include "game.h"
+
+namespace SudoUno {
+    namespace game {
+
+        void Game::dealCards(Player p) {
+            
+        }
+
+        card::Card Game::pullCard() {
+
+        }
+
+        void Game::Start() {
+
+        }
+
+        void Game::End(string reason) {
+
+        }
+
+        void Game::TakeTurn(Player p, card::Card c, bool saidSudo, bool pickUp) {
+
+        }
+    }
+}
