5da430fbf76227e58059c16fe36699ddacfbe24c
Dylan Abbinett <dabbinet@uwo.ca>
2024-04-07 01:59:32 -0400

    Feat: dealCards and pullCard methods added

--- Server/game/game.cpp
+++ Server/game/game.cpp
@@ -5,10 +5,27 @@ namespace SudoUno {
 
         void Game::dealCards(Player p) {
             // TODO: implement me!
+
+            //Deal 7 cards to each player
+            for (int i = 0; i < 7; i++) {
+                //Gets a card from the deck and adds it to the player's hand
+                p.addCard(pullCard());
+            }
         }
 
         card::Card Game::pullCard() {
             // TODO: implement me!
+
+            //Pull a card from the deck
+            std::vector<card::Card> deck;
+
+            //Get card from deck at random
+            int cardIndex = rand() % deck.size();
+            card::Card c = deck[cardIndex];
+            //Remove from deck
+            deck.erase(deck.begin() + cardIndex);
+            //Return card
+            return c;
         }
 
         void Game::Start() {
