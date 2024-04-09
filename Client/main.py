336d770413809ace0dd275e39ca6de25571dd4b8
Pratik Gupta <pratikngupta@outlook.com>
2024-04-09 11:41:51 -0400

    feat: client now take care of sending and reciving the data to and from server, handle the edge case like lobby end and winning

--- Client/main.py
+++ Client/main.py
@@ -54,7 +54,6 @@ def lobby_loop(connection):
     # Lobby Loop (checks server every iteration to see if it should start the game)
     print(f"Welcome {client_name}!")
 
-
     while True:
         clear_terminal()
         print(f"Welcome {client_name}!")
@@ -86,20 +85,54 @@ def game_loop(connection):
         clear_terminal()
 
         # Update game state
-        # TODO: con. function to get the players_map object
-        # TODO: con. function to get the client_hand array
-        # TODO: con. function to get the current_card string
-        # TODO: con. function to get the current_card string
-        # TODO: con. function to get the current_card string
+        data = connection.receive()
+        if "Game.state.update" in data:
+            for line in data.split("\n"):
+                if "players" in line:
+                    players_map = {
+                        player.split("|")[0].strip("“”"): int(player.split("|")[1])
+                        for player in line.split("\n")[1:]
+                    }
+                elif "your_cards" in line:
+                    client_hand = [card.strip("“”") for card in line.split("\n")[1:]]
+                elif "current_card" in line:
+                    current_card = line.split(" = ")[1].strip("“”")
+                elif "turn" in line:
+                    turn = line.split(" = ")[1].strip("“”")
 
         # Check if it's the player's turn
         if turn == client_name:
             printOptions()
             printUI(players_map, client_name, client_hand, current_card, turn, True)
             player_choice = input("Enter your choice: ")
-            # Process player choice on the server
-            # TODO: Implement code here..
-            # Update game state for the 3 globle variables below
+
+            if player_choice == "1":  # Assume "1" means pick up a card
+                data = connection.send(
+                    ["Turn.take", 'card = ""', "pick = true", "sudo = false"]
+                )
+            elif player_choice == "2":  # Assume "2" means place down a card
+                card_to_play = input(
+                    "Enter the card to play: "
+                )  # Get the card to play from the user
+                data = connection.send(
+                    [
+                        "Turn.take",
+                        f'card = "{card_to_play}"',
+                        "pick = false",
+                        "sudo = false",
+                    ]
+                )
+
+            # Handle server response
+            if "Turn.approve" in data:
+                print("Your turn was approved.")
+                if "You forgot to say sudo." in data:
+                    print("You forgot to say sudo.")
+            elif "Turn.reject" in data:
+                reason = data.split("reason: ")[1]
+                print(f"Your turn was rejected. Reason: {reason}")
+
+            # Update game state for the 3 global variables below
             handlePlayerChoice(player_choice, client_hand, client_name)
 
         else:
@@ -110,8 +143,31 @@ def game_loop(connection):
         time.sleep(2)
 
 
-if __name__ == "__main__":
-    # Play again loop
+def lobby_end(data):
+    if "Lobby.end" in data:
+        if "ragequit" in data:
+            name = data.split("player ")[1].split(" ragequit")[0]
+            print(f"The game ended because player {name} ragequit.")
+        elif "placed their last card and won" in data:
+            name = data.split("player ")[1].split(" placed their last card and won")[0]
+            print(
+                f"The game ended because player {name} placed their last card and won."
+            )
+        elif "unexplained error on the server" in data:
+            print(
+                "The game ended because there was an unexplained error on the server."
+            )
+
+        # Play again?
+        if "y" == input("play again?"):
+            main()
+        else:
+            # end the game
+            print("Done.")
+            sys.exit()
+
+
+def main():
     while True:
         handerGameInit()
 
@@ -132,3 +188,7 @@ if __name__ == "__main__":
         else:
             break
     print("Done.")
+
+
+if __name__ == "__main__":
+    main()
